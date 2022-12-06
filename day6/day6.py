bdb42bf60d49f4daf6526ff258af5795e15584e5
jromer <j.p.l.romer@student.tudelft.nl>
2022-12-06 11:59:27 +0100

    day 6

--- day6/day6.py
+++ day6/day6.py
@@ -1,12 +1,12 @@
 #!/bin/python
 
-def firstMarker(chars: list[str], length: int) -> int:
+def firstMarker(chars: str, length: int) -> int:
     for i in range(len(chars)):
         if len(set(chars[i:i+length])) == length:
             return i + length
     return -1
 
-chars = list(open("input", "r").read())
+chars = open("input", "r").read()
 part1 = firstMarker(chars, 4)
 part2 = firstMarker(chars, 14)
 print(part1, part2)
