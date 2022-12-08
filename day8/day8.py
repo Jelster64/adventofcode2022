c7bb50dda0eb290cc030e8cb2ad6d31c699725bf
jromer <j.p.l.romer@student.tudelft.nl>
2022-12-08 16:25:20 +0100

    day 8

--- day8/day8.py
+++ day8/day8.py
@@ -3,58 +3,42 @@ import copy
 from functools import reduce
 
 def isTreeVisible(i: int, j: int, trees: list[list[int]]) -> bool:
-    if i == 0 or j == 0 or i == len(trees)-1 or j == len(trees[i])-1:
+    if i == 0 or j == 0 \
+        or i == len(trees)-1 \
+        or j == len(trees[i])-1:
         return True
     height = trees[i][j]
     return max(trees[i][j+1:]) < height \
-    or max(trees[i][:j]) < height \
-    or max(map(lambda a: a[j], trees[i+1:])) < height \
-    or max(map(lambda a: a[j], trees[:i])) < height
+        or max(trees[i][:j]) < height \
+        or max(map(lambda a: a[j], trees[i+1:])) < height \
+        or max(map(lambda a: a[j], trees[:i])) < height
 
-def calculateScenicScore(i: int, j: int, trees: list[list[int]]) -> int:
+def lineOfSightScore(lineOfSight: list[int], height: int) -> int:
+    score = 0
+    for tree in lineOfSight:
+        score += 1
+        if tree >= height:
+            return score
+    return score
+
+def scenicScore(i: int, j: int, trees: list[list[int]]) -> int:
     height = trees[i][j]
-    score = [0 for _ in range(4)]
-
-    index = i - 1
-    while index >= 0:
-        score[0] += 1
-        if trees[index][j] >= height:
-            break
-        index -= 1
-
-    index = i + 1
-    while index < len(trees[i]):
-        score[1] += 1
-        if trees[index][j] >= height:
-            break
-        index += 1
-
-    index = j - 1
-    while index >= 0:
-        score[2] += 1
-        if trees[i][index] >= height:
-            break
-        index -= 1
-
-    index = j + 1
-    while index < len(trees[i]):
-        score[3] += 1
-        if trees[i][index] >= height:
-            break
-        index += 1
-
-    return reduce(lambda x, y: x * y, score)
+    linesOfSight = [list(reversed(trees[i][:j])),\
+                    trees[i][j+1:],\
+                    list(reversed([trees[k][j] for k in range(i)])),\
+                    [trees[k][j] for k in range(i+1, len(trees[i]))]]
+    return reduce(lambda x, y: x * y, map(lambda x: lineOfSightScore(x, height), linesOfSight))
 
 with open("input", "r") as f:
     trees = list(map(lambda row: list(map(int, row)), f.read().splitlines()))
-res = copy.deepcopy(trees)
+visible = copy.deepcopy(trees)
 scenic = copy.deepcopy(trees)
 
 for i in range(len(trees)):
     for j in range(len(trees[i])):
-        res[i][j] = isTreeVisible(i, j, trees)
-        scenic[i][j] = calculateScenicScore(i, j, trees)
+        visible[i][j] = isTreeVisible(i, j, trees)
+        scenic[i][j] = scenicScore(i, j, trees)
 
-part1 = sum(map(lambda x: sum(list(map(int, x))), res))
+part1 = sum(map(lambda x: sum(list(map(int, x))), visible))
 part2 = max(map(max, scenic))
 print(part1, part2)
