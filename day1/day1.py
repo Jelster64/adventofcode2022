a3ab23c9a8bad028e1298d6552c1fa99d3bbcccd
jromer <j.p.l.romer@student.tudelft.nl>
2022-12-13 17:05:14 +0100

    day 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,4 @@
 #!/bin/python
 with open("input", "r") as f:
-    elves = sorted(list(map(lambda x: sum(map(int, x)), map(str.splitlines, f.read().split("\n\n")))))
+    elves = sorted(map(lambda x: sum(map(int, x)), map(str.splitlines, f.read().split("\n\n"))))
 print(max(elves), sum(elves[-3:]))
